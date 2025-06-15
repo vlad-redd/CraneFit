5cd15d54885f5c3280cb642b8c87b6a6e9df047e
Vishwajeet <manevishwajeet1@gmail.com>
2025-06-15 13:03:15 +0100

    (map) feat

--- app.ts
+++ app.ts
@@ -8,6 +8,8 @@ export function hello() { return "Hi" }
 
 export class CraneFit { 
     openPlans: OpenPlans
+    tileImage: Base64URLString = "";
+
     constructor(container : HTMLDivElement) {
         this.openPlans = new OpenPlans(container)
     }
@@ -23,6 +25,29 @@ export class CraneFit {
         //@ts-ignore
         this.openPlans.addCustomObject(crane)
         return crane
+    }
+
+
+    generateTile(latitude: any, longitude: any, zoom: any) {
+        const requestOptions: any = {
+            method: "GET",
+            redirect: "follow"
+        };
+
+        const mapboxUrl = `https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v12/static/${latitude},${longitude},${zoom},0/300x200@2x?access_token=pk.eyJ1IjoibWFuZXZpc2h3YWplZXQxIiwiYSI6ImNsN2NrYnJiZzB0bWkzb254eG9nMjc4ZWkifQ.62_f1MIY7KL8MXJTyZUGkw`;
+
+        fetch(mapboxUrl, requestOptions)
+        .then((res) => res.blob()) // ✅ Fetch as blob
+        .then((blob) => {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                const dataUrl = reader.result;
+                if (!dataUrl || typeof dataUrl !== 'string') return;
+                this.openPlans.addImagePlane(dataUrl);
+            };
+            reader.readAsDataURL(blob);
+        })
+        .catch(console.error);
     }
 
 }
